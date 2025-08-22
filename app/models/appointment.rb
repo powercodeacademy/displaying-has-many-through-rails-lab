346c68732a9629e64ac675edf566a1057f837973
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-22 09:55:11 -0400

    Format appointment datetime

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor 
   belongs_to :patient 
+
+  def formatted_datetime 
+    self.appointment_datetime.strftime('%B %e, %Y at %H:%M')
+  end
 end
