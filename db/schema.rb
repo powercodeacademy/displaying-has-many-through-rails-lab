de920e5c59763851f4e7453fa926acb1fd951a07
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-26 15:58:05 -0400

    Migrate tables and create associations

---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+ActiveRecord::Schema[7.1].define(version: 2025_08_26_194946) do
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "appointment_datetime"
+    t.integer "doctor_id"
+    t.integer "patient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "department"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
